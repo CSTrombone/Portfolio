48ba0285040aa5f2203740f1de727665f7f824f1
Austin Oden <tboneoden+github@gmail.com>
2024-09-26 04:03:36 -0500

    Modified for gh-pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
 
   return (
       <nav className = {`${sticky ? styles.sticky : styles.navbar}`}>
-        <a className = {styles.title} href = "/"> Portfolio </a>
+        <a className = {styles.title} href = "./"> Portfolio </a>
         <div className = {styles.menu}>
           <img 
               className = {styles.menuBtn} 
