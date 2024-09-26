057519f7cd835043bbe74a6baca50ec1dfd8ccf7
Austin Oden <tboneoden+github@gmail.com>
2024-09-26 03:55:23 -0500

    Modified for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"./Portfolio",
   plugins: [react()],
   css: {
     modules: {
-      base:"{Portfolio}",
       localsConvention: "camelCase"
     }
   }
